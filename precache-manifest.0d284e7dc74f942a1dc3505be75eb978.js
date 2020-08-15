78d0d70fa80e8477076b0dcdabe01864b4428658
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-08-15 13:27:39 +0200

    Updates

---
+++ precache-manifest.0d284e7dc74f942a1dc3505be75eb978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331308c05b8c3303ddfba7b923efd07d",
+    "url": "/simple-todo-app-react/index.html"
+  },
+  {
+    "revision": "951743e50b5d06262095",
+    "url": "/simple-todo-app-react/static/js/2.13bdbf49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-todo-app-react/static/js/2.13bdbf49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66da0adba9c552754729",
+    "url": "/simple-todo-app-react/static/js/main.8de49d8c.chunk.js"
+  },
+  {
+    "revision": "ceb209b91eebb99b0c3d",
+    "url": "/simple-todo-app-react/static/js/runtime-main.e34faef7.js"
+  }
+]);
